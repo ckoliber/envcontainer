d8d96c6db0613cef2ec46a27552e96f2c2a22189
Hoomaan <ckoliber@gmail.com>
2026-05-14 21:58:29 +0200

    feat: add build script versioning logic

--- .github/build.sh
+++ .github/build.sh
@@ -1,9 +1,44 @@
 #!/usr/bin/env bash
 set -e
 
-VERSION=$(git cliff --unreleased --bumped-version)
-if [ "$CI_COMMIT_REF_PROTECTED" != "true" ]; then
-  VERSION=$VERSION-build.$(git rev-parse --short HEAD)
+function get_ref() {
+  TYPE="branch"
+  if [ "${GITHUB_REF_TYPE:-}" = "tag" ]; then
+    TYPE="tag"
+  elif [ -n "${CI_COMMIT_TAG:-}" ]; then
+    TYPE="tag"
+  elif git describe --tags --exact-match >/dev/null 2>&1; then
+    TYPE="tag"
+  fi
+
+  if [ -n "${GITHUB_REF_NAME:-}" ]; then
+    echo "$TYPE/$GITHUB_REF_NAME"
+  elif [ -n "${CI_COMMIT_REF_NAME:-}" ]; then
+    echo "$TYPE/$CI_COMMIT_REF_NAME"
+  else
+    echo "$TYPE/$(git symbolic-ref --quiet --short HEAD || git describe --tags --exact-match 2>/dev/null || git rev-parse --short HEAD)"
+  fi
+}
+
+function is_tag() {
+  [[ "$(get_ref)" =~ ^tag/(.*)$ ]]
+}
+
+function is_release() {
+  [[ "$(get_ref)" =~ ^branch/(main|master)$ ]]
+}
+
+function is_prerelease() {
+  [[ "$(get_ref)" =~ ^branch/(alpha|beta|rc|next)$ ]]
+}
+
+VERSION=""
+if is_tag; then
+  VERSION=$(get_ref | sed 's|^tag/||')
+elif is_release || is_prerelease; then
+  VERSION=$(git cliff --unreleased --bumped-version)
+else
+  VERSION=$(git cliff --unreleased --bumped-version)-build.$(git rev-parse --short HEAD)
 fi
 
 BUILDX=docker-cli-plugin-docker-buildx
